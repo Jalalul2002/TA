1d33da4ac3e5104d68e921c11b93bd7cde3bc13b
Jalalul Mu'ti <jalalul2000@gmail.com>
2024-07-03 21:58:43 +0700

    Data Staff Controller Edit

--- app/Http/Controllers/DataStaffController.php
+++ app/Http/Controllers/DataStaffController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DataStaffController extends Controller
 {
-    public function index() {
-        return view('admin.staff');
+    public function index()
+    {
+
+        $users = User::where('role', 'staff')->get();
+        return view('admin.staff', compact('users'));
+    }
+
+    public function show($id)
+    {
+        $user = User::where('role', 'staff')->findOrFail($id);
+        return view('users.show', compact('user'));
     }
 }

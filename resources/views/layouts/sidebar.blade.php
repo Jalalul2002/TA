84a39d1de722630b19a2d71ce099ffe34a73c48c
Jalalul Mu'ti <jalalul2000@gmail.com>
2025-02-11 00:34:26 +0700

    Routing User

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,8 +4,8 @@
     @php
         $menus = [
             [
-                'route' => Auth::user()->usertype == 'admin' ? route('admin.dashboard') : route('dashboard'),
-                'isActive' => Request::routeIs(['dashboard', 'admin.dashboard']),
+                'route' => Auth::user()->usertype == 'admin' ? route('admin.dashboard') : (Auth::user()->usertype == 'user' ? route('user.dashboard') : route('dashboard')),
+                'isActive' => Request::routeIs(['dashboard', 'admin.dashboard', 'user.dashboard']),
                 'icon' =>
                     '<div class="p-2 bg-uinGreen group-hover:bg-uinBlue rounded-lg transition duration-500"><svg class="size-4 fill-white" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M7,0H4A4,4,0,0,0,0,4V7a4,4,0,0,0,4,4H7a4,4,0,0,0,4-4V4A4,4,0,0,0,7,0ZM9,7A2,2,0,0,1,7,9H4A2,2,0,0,1,2,7V4A2,2,0,0,1,4,2H7A2,2,0,0,1,9,4Z"/><path d="M20,0H17a4,4,0,0,0-4,4V7a4,4,0,0,0,4,4h3a4,4,0,0,0,4-4V4A4,4,0,0,0,20,0Zm2,7a2,2,0,0,1-2,2H17a2,2,0,0,1-2-2V4a2,2,0,0,1,2-2h3a2,2,0,0,1,2,2Z"/><path d="M7,13H4a4,4,0,0,0-4,4v3a4,4,0,0,0,4,4H7a4,4,0,0,0,4-4V17A4,4,0,0,0,7,13Zm2,7a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V17a2,2,0,0,1,2-2H7a2,2,0,0,1,2,2Z"/><path d="M20,13H17a4,4,0,0,0-4,4v3a4,4,0,0,0,4,4h3a4,4,0,0,0,4-4V17A4,4,0,0,0,20,13Zm2,7a2,2,0,0,1-2,2H17a2,2,0,0,1-2-2V17a2,2,0,0,1,2-2h3a2,2,0,0,1,2,2Z"/></svg></div>',
                 'label' => 'Dashboard',
--- routes/web.php
+++ routes/web.php
@@ -13,56 +13,62 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
+require __DIR__ . '/auth.php';
+
 Route::get('/', function () {
     // return redirect('/login');
     return view('landing');
 });
 
-Route::middleware(['auth', 'orMiddleware'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/data-product', [ProductController::class, 'index'])->name('product');
-    Route::get('/add-product', [ProductController::class, 'add'])->name('add-product');
-    Route::post('/add-product', [ProductController::class, 'store']);
-    Route::delete('/data-product/{id}', [ProductController::class, 'destroy'])->name('destroy-product');
-
     Route::get('/data-aset', [AssetController::class, 'indexInv'])->name('data-aset');
-    Route::get('/add-aset-inventaris', [AssetController::class, 'addInv'])->name('add-aset-inv');
-    Route::post('/add-aset-inventaris', [AssetController::class, 'store']);
     Route::get('/data-barang-habis-pakai', [AssetController::class, 'indexBhp'])->name('data-bhp');
-    Route::get('/add-aset-bhp', [AssetController::class, 'addBhp'])->name('add-aset-bhp');
-    Route::post('/add-aset-bhp', [AssetController::class, 'store']);
-    Route::get('/edit-aset/{product_code}', [AssetController::class, 'edit'])->name('edit-aset');
-    Route::put('/update-aset/{product_code}', [AssetController::class, 'update'])->name('update-aset');
-    Route::delete('/delete-aset/{product_code}', [AssetController::class, 'destroy'])->name('destroy-aset');
     Route::get('/export-inv', [AssetController::class, 'exportInv'])->name('export.inv');
     Route::get('/export-bhp', [AssetController::class, 'exportBhp'])->name('export.bhp');
 
     Route::get('/perencanaan-inv', [PerencanaanController::class, 'indexInv'])->name('perencanaan-inv');
     Route::get('/perencanaan-bhp', [PerencanaanController::class, 'indexBhp'])->name('perencanaan-bhp');
+    Route::get('/detail-perencanaan/{id}', [PerencanaanController::class, 'show'])->name('detail-perencanaan');
+    Route::get('/perencanaan/{id}/download', [PerencanaanController::class, 'download'])->name('perencanaan.download');
+
+    Route::post('/transaction-bhp', [TransactionController::class, 'index']);
+
+    Route::get('/prediksi', [PredictionController::class, 'index'])->name('prediksi');
+});
+
+Route::middleware(['auth', 'orMiddleware'])->group(function () {
+    Route::get('/add-product', [ProductController::class, 'add'])->name('add-product');
+    Route::post('/add-product', [ProductController::class, 'store']);
+    Route::delete('/data-product/{id}', [ProductController::class, 'destroy'])->name('destroy-product');
+
+    Route::get('/add-aset-inventaris', [AssetController::class, 'addInv'])->name('add-aset-inv');
+    Route::post('/add-aset-inventaris', [AssetController::class, 'store']);
+    Route::get('/add-aset-bhp', [AssetController::class, 'addBhp'])->name('add-aset-bhp');
+    Route::post('/add-aset-bhp', [AssetController::class, 'store']);
+    Route::get('/edit-aset/{product_code}', [AssetController::class, 'edit'])->name('edit-aset');
+    Route::put('/update-aset/{product_code}', [AssetController::class, 'update'])->name('update-aset');
+    Route::delete('/delete-aset/{product_code}', [AssetController::class, 'destroy'])->name('destroy-aset');
+
     Route::get('/add-perencanaan-inv', [PerencanaanController::class, 'createInv'])->name('add-perencanaan.inv');
     Route::post('/add-perencanaan-inv', [PerencanaanController::class, 'store']);
     Route::get('/add-perencanaan-bhp', [PerencanaanController::class, 'createBhp'])->name('add-perencanaan.bhp');
     Route::post('/add-perencanaan-bhp', [PerencanaanController::class, 'store']);
-    Route::get('/detail-perencanaan/{id}', [PerencanaanController::class, 'show'])->name('detail-perencanaan');
     Route::delete('/delete-perencanaan/{id}', [PerencanaanController::class, 'destroy'])->name('destroy-rencana');
     Route::delete('/perencanaan-bhp/destroy-item/{id}', [PerencanaanController::class, 'destroyItem'])->name('rencana.destroy-item');
     Route::post('/perencanaan-bhp/add-item/{id}', [PerencanaanController::class, 'storeItem'])->name('rencana.store-item');
     Route::get('/edit-rencana/{id}', [PerencanaanController::class, 'edit'])->name('rencana.edit-rencana');
     Route::put('/update-rencana/{id}', [PerencanaanController::class, 'update'])->name('rencana.update-rencana');
-    Route::get('/perencanaan/{id}/download', [PerencanaanController::class, 'download'])->name('perencanaan.download');
     Route::post('/perencanaan/{id}/complete', [PerencanaanController::class, 'complete'])->name('perencanaan.complete');
 
-    Route::post('/transaction-bhp', [TransactionController::class, 'index']);
     Route::post('/add-transaction-bhp', [TransactionController::class, 'create'])->name('add-transaction.bhp');
 
-    Route::get('/prediksi', [PredictionController::class, 'index'])->name('prediksi');
     Route::post('/send-data', [PredictionController::class, 'sendData']);
 });
-
-require __DIR__ . '/auth.php';
 
 //Staff Route
 Route::middleware(['auth', 'staffMiddleware'])->group(function () {
